7b3804bd149b5ed7bb6ca737b517d8fcb9ebf6cd
Masseteau Emma <n.masseteau@student.isartdigital.com>
2026-02-19 14:21:12 +0100

    Add math submodule

--- Readme.md
+++ Readme.md
@@ -1,11 +1,11 @@
+Update submodule
+```bash
+git submodule update --init --recursive
+```
+
 \## Generate Visual Studio Solution (Windows)
 
-
-
 To generate a Visual Studio `.sln` using xmake:
-
-
-
 ```bash
 
 xmake project -k vsxmake
--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,23 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.vsxmake.autoupdate")
 
+add_requires("glfw")
+add_requires("stb")
+
+add_packages("glfw")
+add_packages("stb")
+
 target("DX12Renderer")
     set_kind("binary")
     add_files("src/*.cpp")
-    add_files("src/*.h")
-    add_files("src/*.hpp")
+    add_headerfiles("src/*.h")
+    add_headerfiles("src/*.hpp")
 
+    -- Third Party
+    add_headerfiles("ThirdParty/(**.h)")
+    add_headerfiles("ThirdParty/(**.hpp)")
+    add_headerfiles("ThirdParty/(**.c)")
+    add_files("ThirdParty/(**.cpp)")
+
+    add_includedirs("ThirdParty", {public = true})
+target_end()
